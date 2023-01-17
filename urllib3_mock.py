2c7c3278c738716d78506ea90fb9a1a4f3ecc40d
Daniel Wallace <danielwallace@gtmanfred.com>
2023-01-17 14:58:10 -0500

    upgrade for python3.11

--- urllib3_mock.py
+++ urllib3_mock.py
@@ -40,15 +40,15 @@ __all__ = ['Responses']
 
 
 def get_wrapped(func, wrapper_template, evaldict):
-    # Preserve the argspec for the wrapped function so that testing
-    # tools such as pytest can continue to use their fixture injection.
-    args, a, kw, defaults = inspect.getargspec(func)
+    signature = inspect.signature(func)
 
-    signature = inspect.formatargspec(args, a, kw, defaults)
     is_bound_method = hasattr(func, '__self__')
     if is_bound_method:
         args = args[1:]     # Omit 'self'
-    callargs = inspect.formatargspec(args, a, kw, None)
+    callargs = inspect.Signature(parameters=[
+        inspect.Parameter(name=name, kind=inspect.Parameter.POSITIONAL_OR_KEYWORD)
+        for name in signature.parameters
+    ])
 
     ctx = {'signature': signature, 'funcargs': callargs}
     _exec(wrapper_template % ctx, evaldict)
